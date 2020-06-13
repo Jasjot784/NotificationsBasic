172b963f91f0055e93fd01dc36ab321e34c24b5c
Jasjot Singh <jasjot784@gmail.com>
2020-06-13 19:41:09 +0530

    Notification BAsics 3

--- app/src/main/java/com/jasjotsingh/notificationbasic/MainActivity.java
+++ app/src/main/java/com/jasjotsingh/notificationbasic/MainActivity.java
@@ -1,16 +1,18 @@
 package com.jasjotsingh.notificationbasic;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import static com.jasjotsingh.notificationbasic.App.CHANNEL_1_ID;
 import static com.jasjotsingh.notificationbasic.App.CHANNEL_2_ID;
@@ -33,18 +35,23 @@ public class MainActivity extends AppCompatActivity {
         String title = etTitle.getText().toString();
         String message = etMessage.getText().toString();
 
-        Intent activityIntent = new Intent(this,MainActivity.class);
-        PendingIntent contentIntent = PendingIntent.getActivity(this,0,activityIntent,0);
+        Intent activityIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, activityIntent, 0);
 
-        Intent broadcastIntent = new Intent(this,NotificationReceiver.class);
-        broadcastIntent.putExtra("toastMessage",message);
-        PendingIntent actionIntent = PendingIntent.getBroadcast(this,0,broadcastIntent,PendingIntent.FLAG_UPDATE_CURRENT);//Will Update put extra
+        Intent broadcastIntent = new Intent(this, NotificationReceiver.class);
+        broadcastIntent.putExtra("toastMessage", message);
+        PendingIntent actionIntent = PendingIntent.getBroadcast(this, 0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT);//Will Update put extra
 
-
-        Notification notification = new NotificationCompat.Builder(this,CHANNEL_1_ID)
+        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_phone);
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
                 .setSmallIcon(R.drawable.ic_one)
                 .setContentTitle(title)
                 .setContentText(message)
+                .setLargeIcon(largeIcon)
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(getString(R.string.long_dummy_text))
+                        .setBigContentTitle("Big Content Title")
+                        .setSummaryText("Summary Text"))
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
                 .setColor(Color.BLUE)
@@ -62,10 +69,17 @@ public class MainActivity extends AppCompatActivity {
         String title = etTitle.getText().toString();
         String message = etMessage.getText().toString();
 
-        Notification notification = new NotificationCompat.Builder(this,CHANNEL_2_ID)
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
                 .setSmallIcon(R.drawable.ic_two)
                 .setContentTitle(title)
                 .setContentText(message)
+                .setStyle(new NotificationCompat.InboxStyle()
+                        .addLine("This is line 1")
+                        .addLine("This is line 2")
+                        .addLine("This is line 3")
+                        .addLine("This is line 4")
+                        .setBigContentTitle("Big Content Title")
+                        .setSummaryText("Summary Text"))
                 .setPriority(NotificationCompat.PRIORITY_LOW)
                 .build();
 
